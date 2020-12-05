6ba8cbf09234b346c2797c3cf78a8430dd24fb50
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-05 15:33:49 +0800

    Brew formula update for gojira version v1.2.1

--- Formula/gojira.rb
+++ Formula/gojira.rb
@@ -2,16 +2,16 @@
 class Gojira < Formula
   desc "Save your time on Jira operation through the command line."
   homepage "https://github.com/leozhantw/gojira"
-  version "1.2.0"
+  version "1.2.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/leozhantw/gojira/releases/download/v1.2.0/gojira_1.2.0_Darwin_x86_64.tar.gz"
-    sha256 "b6967f685169a8bf1c3c67251c0c62ff8dbe4a04e074ee5a20d85c427f389c9f"
+    url "https://github.com/leozhantw/gojira/releases/download/v1.2.1/gojira_1.2.1_Darwin_x86_64.tar.gz"
+    sha256 "01f42e5cf0f1db214c9ac65304a1c781f063f051ce30e6e354bbf9801519f058"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/leozhantw/gojira/releases/download/v1.2.0/gojira_1.2.0_Linux_x86_64.tar.gz"
-    sha256 "9ec1a9ba7b30743aebdd0bbb7ede13e5aa8d57404c8047d929139b1be8ec3906"
+    url "https://github.com/leozhantw/gojira/releases/download/v1.2.1/gojira_1.2.1_Linux_x86_64.tar.gz"
+    sha256 "065ce106fd1490d9d7b40dd8696a12901e49dfdef8c0edab3f08e9be8c5a079e"
   end
 
   def install
